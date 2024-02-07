100873cca4ae337e13889f60c8ecc73691b20802
1034205079 <47485084+1034205079@users.noreply.github.com>
2024-02-07 19:38:23 +0800

    Update config.py

--- config/config.py
+++ config/config.py
@@ -6,15 +6,15 @@ class Config:
     project_path = os.path.split(os.path.split(__file__)[0])[0]
     project_name = str(project_path).split('\\')[-1]  # 从路径中拿到项目名
     windows_ip = "172.16.10.34"
-    base_url = "http://novel.52toolbox.com"
+    base_url = "http://novel.123.com"
     db_info = {"host": "172.16.10.180", "port": 3306, "user": "root", "password": "root", "db": "kdt_test"}
     logger_name = "biglogger"
     logger_level = logging.DEBUG
-    mail_account = "songxiaofei@ludashi.com"
-    mail_token = "lq6ugG9jg1z4d4mg"
+    mail_account = "123@123.com"
+    mail_token = "12345"
     mail_smtp = "smtp.qiye.aliyun.com"
     mail_port = 465
-    receive_mail = ['songxiaofei@ludashi.com', ]  # 收件人列表
+    receive_mail = ['123@123.com', ]  # 收件人列表
 
 
 if __name__ == '__main__':
