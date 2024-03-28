d5982264bdefc62695511ae89e633e8284914784
songxiaofei <1034205079@qq.com>
2024-03-28 14:18:28 +0800

    更新excel阅读注释

--- runner_api.py
+++ runner_api.py
@@ -5,7 +5,7 @@ from Novel_KDT_TestFrame.tools.DBClass import DBClass
 from Novel_KDT_TestFrame.tools.MyLogger import mylogger  # 直接导入实例化
 
 
-class Runner(object):
+class Runner:
     library_instance = {}  # 存储实例化的关键字对象 {“关键字”:关键字对象实例,....}
 
     def __init__(self):
@@ -19,18 +19,16 @@ class Runner(object):
             keyword_path = f"{Config.project_name}.keyword"  # 关键字路径
             keyword_path_module = f"{keyword_path}.{module_name}"  # 关键字和模块名拼接在一起
             __import__(keyword_path_module)  # 导入模块， 模块名是字符串，并且是完整的路径（从根目录开始导入）
-            md = sys.modules[keyword_path_module]
-            # 2.导入类
+            md = sys.modules[keyword_path_module]  # 获取模块对象
             # 我设计的关键字模块名和类 同名字
             keyword_cls = getattr(md, module_name)  # 从模块里面拿类
             # 3.实例化
             instance = keyword_cls()  # 我的设计是构造方法不需要传入参数，实例化类
             mylogger.info("新创建了一个关键字实例-->" + str(instance))
-            # 4.保存一下
-            cls.library_instance[library_name] = instance
+            cls.library_instance[library_name] = instance  # 保存实例化的对象
         return cls.library_instance[library_name]
 
-    def run_one_case(self, case=None):
+    def run_one_case(self, case=None):  # 运行单个用例
 
         mylogger.info(
             f"开始执行用例-->{case.get('用例编号')}  -->{case.get('用例模块')}-->{case.get('用例功能')}-->{case.get('用例标题')}")
@@ -41,12 +39,9 @@ class Runner(object):
             for step in case.get("测试步骤"):
                 mylogger.info("开始执行步骤-->" + step[0])
                 try:
-                    # 3.获取实例
-                    instance = self.get_library_instance(step[1])
-                    # 4.获取实例上的方法
-                    method = getattr(instance, step[2])
-                    # 5.运行实例方法
-                    method(*step[3:])  # 传入必要的参数
+                    instance = self.get_library_instance(step[1])  # 实列化keywrod里封装的library，excel中的“库”列
+                    method = getattr(instance, step[2])  # 获取实例上的方法，表格中的“关键字”列
+                    method(*step[3:])  # 运行实例方法，传入必要的参数
                 except:
                     mylogger.info("  步骤执行  失败")
                     case["执行结果"] = "fail"
@@ -61,7 +56,7 @@ class Runner(object):
         mylogger.info(f"结束执行用例-->{case.get('用例编号')} -->  {case['执行结果']}")
         mylogger.info("-" * 60)
 
-    def run_all_case(self, case_list):
+    def run_all_case(self, case_list):  # 运行所有用例
         self.case_list = case_list  # 绑定在实例上
         for case in self.case_list:
             self.run_one_case(case)
@@ -87,15 +82,16 @@ class Runner(object):
         self.db.update("insert into result(report_time,case_total,case_pass,case_fail,case_skip) "
                        f"values('{self.report_time}',{case_total},{case_pass},{case_fail},{case_skip})")
         # 2.插入用例执行信息结果
-
         for case in self.case_list:
-            # 遇到一个问题：用例的错误信息中有引号，打乱了sql语句，导致sql不能执行。
-            # 解决：存入的sql信息最后需要在html中呈现，因此可以进行html转码，去掉引号。
-            case_error_info = html.escape(case.get('执行信息'))
-            case_error_info = case_error_info.replace("\n", "<br>").replace("\\", "\\\\")  # 优化页面html显示格式
+            """ 遇到一个问题：用例的错误信息中有引号，打乱了sql语句，导致sql不能执行。
+            解决：存入的sql信息最后需要在html中呈现，因此可以进行html转码，去掉引号。"""
+            case_error_info = html.escape(case.get('执行信息'))  # 转义错误信息，防止sql语句错误
+            case_error_info = case_error_info.replace("\n", "<br>").replace("\\", "\\\\")  # 换行和反斜杠转义
             self.db.update(
                 'insert into details(report_time,case_no,execute,case_module,case_function,case_title,result,errorinfo)'
-                f'''values("{self.report_time}","{case.get('用例编号')}","{case.get('是否执行')}","{case.get('用例模块')}","{case.get('用例功能')}","{case.get('用例标题')}","{case.get('执行结果')}","{case_error_info}")''')
+                f'''values("{self.report_time}","{case.get('用例编号')}","{case.get('是否执行')}","{case.get('用例模块')}",
+                "{case.get('用例功能')}","{case.get('用例标题')}","{case.get('执行结果')}","{case_error_info}")''')
+            # 保存用例执行信息到数据库
 
     def send_mail(self):
         """发送邮件"""
@@ -119,4 +115,4 @@ if __name__ == '__main__':
     r = Runner()
     r.run_all_case(cases)
     r.show_result()
-    r.send_mail()
+    # r.send_mail()
--- tools/ReadExcleCase.py
+++ tools/ReadExcleCase.py
@@ -3,11 +3,12 @@ import openpyxl
 
 class ReadExcleCase(object):
     def __init__(self, case_file):
-        self.case_file = case_file
-        self.wb = openpyxl.load_workbook(self.case_file)
+        self.case_file = case_file  # 用例文件路径
+        self.wb = openpyxl.load_workbook(self.case_file)  # 加载excel文件
 
     def read_by_sheet(self, sheetname):
-        """根据sheetname读取用例"""
+        """根据sheetname读取用例,
+        测试步骤:[[步骤一的行]，[步骤二的行]]"""
         sheet = self.wb[sheetname]
         sheet_values = list(sheet.values)  # 需要转化成list才看到value
         sheet_header = sheet_values[0]  # 第一行是列名儿，先取出来拿到
@@ -18,12 +19,12 @@ class ReadExcleCase(object):
                 new_case = {header: value for header, value in zip(sheet_header, row)}  # 利用推导式存储字典格式的value
                 new_case[sheet_header[5]] = []  # 每个用例的步骤用列表送入
                 case_list.append(new_case)  # 保存用例信息
-            new_step = [step_info for step_info in row[5:] if step_info is not None]
+            new_step = [step_info for step_info in row[5:] if step_info is not None]  # 步骤信息从第6列开始，取出不为空的部分
             new_case[sheet_header[5]].append(new_step)  # 把用例加入步骤
 
         return case_list
 
-    def read_all_sheets(self):
+    def read_all_sheets(self):  # 读取所有sheet
         """读取所有sheet中的用例"""
         all_sheet_names = self.wb.sheetnames
         case_all_sheet = []
